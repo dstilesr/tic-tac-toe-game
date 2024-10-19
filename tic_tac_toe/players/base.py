5fbe2569eddfc07397a2a96a51ae6b57acd487d5
dstilesr <d.stiles.r@hotmail.com>
2024-10-19 13:05:49 -0500

    Player interface

---
+++ tic_tac_toe/players/base.py
@@ -0,0 +1,62 @@
+from typing import Literal, List
+from abc import ABC, abstractmethod
+
+
+class BasePlayer(ABC):
+    """
+    Interface for a player in the game.
+    """
+
+    def __init__(self, mark: Literal["X", "O"]) -> None:
+        self.__mark = mark
+
+        rival = "O" if self.mark == "X" else "X"
+        self.mapping = {
+            self.mark: "1",
+            rival: "2",
+            "-": "0"
+        }
+
+    @property
+    def mark(self) -> Literal["X", "O"]:
+        """
+        Which 'mark' the player plays.
+        """
+        return self.__mark
+
+    def translate_board(self, state: str) -> str:
+        """
+        Translate the state of the board to a string of 0, 1, 2 characters. 0
+        represents an empty cell, 1 a cell occupied by the player, and 2 a
+        cell occupied by the rival player.
+        :param state:
+        :return:
+        """
+        return "".join(
+            self.mapping[s] for s in state
+        )
+
+    @abstractmethod
+    def make_move(
+            self,
+            reward: float,
+            state: str,
+            available_moves: List[int]) -> int:
+        """
+        Make a move in the game.
+        :param reward: Reward from previous state-action.
+        :param state: Current state of the game.
+        :param available_moves: Available moves in the game.
+        :return: Index (between 0 and 8) of the cell to occupy.
+        """
+        pass
+
+    @abstractmethod
+    def end_game(self, reward: float, state: str):
+        """
+        Register end of the game.
+        :param reward: Final reward.
+        :param state: Final state of the board.
+        :return:
+        """
+        pass
---
+++ tic_tac_toe/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class GameSettings(BaseModel):
+    """
+    Settings for the game.
+    """
+    win_reward: float = 1.0  #: Reward for winning
+    lose_reward: float = -1.0  #: Reward for losing
+    draw_reward: float = 0.0  #: Reward for ending in draw
+    step_reward: float = 0.0  #: Reward at each non-terminal step
