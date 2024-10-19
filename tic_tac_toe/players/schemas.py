b4f52c87d1bf8bdd37775f7e45618383660f611b
dstilesr <d.stiles.r@hotmail.com>
2024-10-19 14:29:29 -0500

    TD Settings schema

---
+++ tic_tac_toe/players/schemas.py
@@ -0,0 +1,33 @@
+from typing import Dict
+from pydantic import BaseModel, Field
+
+
+class TDSettings(BaseModel):
+    """
+    Settings for a general TD-Learner Player.
+    """
+    epsilon: float = Field(
+        default=0.1,
+        ge=0.0,
+        le=1.0,
+    )
+    default_q: float = 0.0
+    discount_rate: float = Field(
+        default=1.0,
+        le=1.0,
+        gt=0.0,
+    )
+    step_size: float = Field(
+        default=0.2,
+        le=1.0,
+        gt=0.0,
+    )
+
+
+class TabularPolicy(BaseModel):
+    """
+    A tabular policy.
+    """
+    states: Dict[str, Dict[int, float]] = Field(
+        description="Map of state -> (action -> q_value)"
+    )
