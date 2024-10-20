a52f1dfba0342fca4b65281b024e51a52a256b5e
dstilesr <d.stiles.r@hotmail.com>
2024-10-19 23:24:43 -0500

    Tests for random player

--- test/test_cases/__init__.py
+++ test/test_cases/__init__.py
@@ -1,1 +1,2 @@
 from .test_game import TestGame
+from .random_player_tests import RandomPlayerTest
---
+++ test/test_cases/random_player_tests.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+
+from tic_tac_toe.players.random import RandomPlayer
+
+
+class RandomPlayerTest(TestCase):
+    """
+    Tests for RandomPlayer class.
+    """
+
+    def test_translate(self):
+        """
+        Test that the board is translated correctly.
+        """
+        board = (
+            "XOX"
+            "OOX"
+            "X--"
+        )
+        x_play = RandomPlayer("X")
+        self.assertEqual(
+            x_play.translate_board(board),
+            "121221100",
+            "Failed to translate board."
+        )
+
+        o_play = RandomPlayer("O")
+        self.assertEqual(
+            o_play.translate_board(board),
+            "212112200",
+            "Failed to translate board."
+        )
+
+    def test_move_selection(self):
+        """
+        Test that moves are selected correctly.
+        """
+        x_play = RandomPlayer("X")
+        self.assertIn(
+            x_play.make_move(0.0, "", [1, 2, 3]),
+            [1, 2, 3],
+            "Selected invalid move."
+        )
+        self.assertEqual(
+            x_play.make_move(0.0, "", [1]),
+            1,
+            "Selected invalid move."
+        )
